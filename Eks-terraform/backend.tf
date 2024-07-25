5281553841cdafb80a2c7394fb1872bc92cbd031
vamshipagidi <121084559+vamshipagidi@users.noreply.github.com>
2024-07-25 12:33:10 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "my-new-bucket-terraform-2024-07" # Replace with your actual S3 bucket name
-    key    = "Jenkins1/terraform.tfstate"
+    bucket = "my-s3-bucket-tf-state-2024-07" # Replace with your actual S3 bucket name
+    key    = "Jenkins/terraform.tfstate"
     region = "us-east-1"
   }
 }
