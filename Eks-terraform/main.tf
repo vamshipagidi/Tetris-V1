8fe3e31f3bc70c9a00507b061c121bc10c4d7afb
vamshipagidi <121084559+vamshipagidi@users.noreply.github.com>
2024-07-26 14:13:24 +0530

    Update main.tf

--- Eks-terraform/main.tf
+++ Eks-terraform/main.tf
@@ -32,16 +32,20 @@ data "aws_subnets" "public" {
     values = [data.aws_vpc.default.id]
   }
 }
-data "aws_availability_zones" "example" {
-  state = "available"
-}
+
 #cluster provision
 resource "aws_eks_cluster" "example" {
   name     = "EKS_CLOUD"
   role_arn = aws_iam_role.example.arn
 
   vpc_config {
-    subnet_ids = data.aws_subnets.public.ids
+    subnet_ids = [
+      "us-east-1a",
+      "us-east-1b",
+      "us-east-1c",
+      "us-east-1d",
+      "us-east-1f",
+    ]
   }
 
   # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
