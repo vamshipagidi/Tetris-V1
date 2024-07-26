4234006999ba16f81c414da8ecac3cdcff6b66f3
vamshipagidi <121084559+vamshipagidi@users.noreply.github.com>
2024-07-26 13:12:11 +0530

    Update main.tf

--- Eks-terraform/main.tf
+++ Eks-terraform/main.tf
@@ -37,10 +37,7 @@ data "aws_subnets" "public" {
 data "aws_availability_zones" "example" {
   all_availability_zones = true
 
-  filter {
-    name   = "opt-in-status"
-    values = ["not-opted-in", "opted-in"]
-  }
+ 
 
 }
 #cluster provision
